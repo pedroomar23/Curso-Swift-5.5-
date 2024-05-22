07b07ec99fbfe85017496bfba39ed0feb83fbc7f
pedroomar23 <pocl9812@gmail.com>
2024-05-22 14:24:26 -0400

    Iniciando los ejercicios

---
+++ Curso - Swfit 5.5/01_hello-world/01_hello_world.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pedro Omar  on 5/22/24.
+//
+
+import Foundation
+
+var course = "Hello Playgrounds"
---
+++ Curso - Swfit 5.5/04_nomenclaturas_variables/04_nomenclaturas_variables.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pedro Omar  on 5/22/24.
+//
+
+import Foundation
